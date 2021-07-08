5960725a284fe625254f1f96062b2063fac2926b
Eduard Larranaga <eduard@Eduards-MacBook-Air.local>
2021-07-08 09:31:07 -0500

    We add a function that calculates the quadrupole tensor components.

--- main/main.py
+++ main/main.py
@@ -14,13 +14,14 @@ r2 = 2*G*m2/c**2 # Schwarzschild radius for m2
 r_merge = r1 + r2 # Separation distance for fusion/collision
 
 M = m1 + m2 # Total mass (solar masses)
+mu = m1*m2/M # Reduced mass
 GM = G*M # gravitational parameter
 
 
 # Initial Values
 E = -70. # energy
 L = 50. # angular momentum
-omega = np.pi/4 # argument of the pericenter
+omega = np.pi/3 # argument of the pericenter
 
 
 
@@ -56,16 +57,21 @@ def ellipse(p, ecc, f):
   r = p/(1-ecc*np.cos(f - omega))
   x = r*np.cos(f)
   y = r*np.sin(f)
-  return x, y, r
+  z = 0.
+  return x, y, z, r
 
+# Quadrupole tensor components
+def qij(x):
+  return mu*(np.outer(x,x) - np.identity(3)*np.dot(x,x)/3)
 
 # Time grid definition
 time = np.zeros(1)
 dt = 1E-4 # timestep
-n = 200000 # number of steps
+n = 80000 # number of steps
 
-position = np.zeros([1,3])
+position = np.zeros([1,4])
 angle = np.zeros(1)
+q = np.zeros([3,3])
 
 # Initial condition in the grid
 angle[0] = 0.
@@ -80,23 +86,24 @@ for i in range(n):
   ecc = eccentricity(E,L)
   p = semilatus(L)
   # Check for the merge of the binary system
-  if position[i,2]<r_merge:
+  if position[i,3]<r_merge:
     print('Binary system merged after {:.5f} years using {:.0f} steps'.format(time[i],i))
-    print('\n The final separation distance is {:.5e} au.\n'.format(position[-1,2]))
+    print('\n The final separation distance is {:.5e} au.\n'.format(position[-1,3]))
     break
 
   time = np.append(time, time[len(time)-1] + dt)
-  angle = np.append(angle, phi(L, position[i,2], angle[i]))
+  angle = np.append(angle, phi(L, position[i,3], angle[i]))
   position = np.append(position, [ellipse(p, ecc, angle[i+1])], axis=0)
+  q = qij(position[i,0:3])
   
   # Check the separation radius to change the time-step
-  if position[i+1,2]>1E-1:
+  if position[i+1,3]>1E-1:
     dt = 1E-4 # time-step back to the original
     r_crit = 1E-1 # critical radius back to the original
-  elif position[i+1,2]<r_crit:
+  elif position[i+1,3]<r_crit:
     dt = 0.01*dt # diminish the time-step
     r_crit = 0.01*r_crit # diminish the critical radius
-  elif position[i+1,2]>r_crit:
+  elif position[i+1,3]>r_crit:
     dt = 100*dt # increase the time-step
     r_crit = 100*r_crit # increase the critical radius
   
@@ -105,6 +112,7 @@ for i in range(n):
   E = E-5*dt
   if L < 0:
     L = 0.
+
 
 plt.figure(figsize=(10,7))
 plt.plot(position[:,0],position[:,1])
