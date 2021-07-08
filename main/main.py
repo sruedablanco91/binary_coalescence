833025f03b8da08bca7b9bb1547ae97abf2e4c66
Eduard Larranaga <eduard@Eduards-MacBook-Air.local>
2021-07-07 21:08:55 -0500

    Semi-major axis function erased. Check for the value of the eccentricity erased. Timestep change routine added

--- main/main.py
+++ main/main.py
@@ -11,7 +11,7 @@ m2 = 7.  # Solar masses
 # Radii
 r1 = 2*G*m1/c**2 # Schwarzschild radius for m1
 r2 = 2*G*m2/c**2 # Schwarzschild radius for m2
-r_crit = r1 + r2 # Separation distance for fusion/collision
+r_merge = r1 + r2 # Separation distance for fusion/collision
 
 M = m1 + m2 # Total mass (solar masses)
 GM = G*M # gravitational parameter
@@ -20,7 +20,7 @@ GM = G*M # gravitational parameter
 # Initial Values
 E = -70. # energy
 L = 50. # angular momentum
-omega = np.pi/3 # argument of the pericenter
+omega = np.pi/4 # argument of the pericenter
 
 
 
@@ -32,12 +32,6 @@ def semilatus(L):
   '''
   return L**2/GM
 
-def a(E):
-  '''
-  Returns the semimajor axis of the orbit 
-  in au from the energy E
-  '''
-  return -GM/(2*E)
 
 def eccentricity(E, L):
   '''
@@ -78,33 +72,39 @@ angle[0] = 0.
 position[0] = ellipse(semilatus(L), eccentricity(E,L), angle[0])
 
 
+# Critical radius to modify dt
+r_crit = 1E-1
 
 # Main Loop
 for i in range(n):
   ecc = eccentricity(E,L)
   p = semilatus(L)
   # Check for the merge of the binary system
-  if position[i,2]<r_crit:
-    print('The binary system has merged after {:.5f} years'.format(time[i]))
-    break
-  # Check for ellipse's eccentricity
-  if ecc<0. or ecc>=1.:
-    print('\n The eccentricity is not that of an ellipse.')
-    print('\n Model stopped after {:.5f} years'.format(time[i]) )
+  if position[i,2]<r_merge:
+    print('Binary system merged after {:.5f} years using {:.0f} steps'.format(time[i],i))
+    print('\n The final separation distance is {:.5e} au.\n'.format(position[-1,2]))
     break
 
   time = np.append(time, time[len(time)-1] + dt)
   angle = np.append(angle, phi(L, position[i,2], angle[i]))
   position = np.append(position, [ellipse(p, ecc, angle[i+1])], axis=0)
+  
   # Check the separation radius to change the time-step
-  if position[i+1,2]<1E-1:
-    dt = 1E-5 # diminish the time-step
-  else:
+  if position[i+1,2]>1E-1:
     dt = 1E-4 # time-step back to the original
+    r_crit = 1E-1 # critical radius back to the original
+  elif position[i+1,2]<r_crit:
+    dt = 0.01*dt # diminish the time-step
+    r_crit = 0.01*r_crit # diminish the critical radius
+  elif position[i+1,2]>r_crit:
+    dt = 100*dt # increase the time-step
+    r_crit = 100*r_crit # increase the critical radius
+  
   #Energy and Angular Momentum lose model 
   L = L-5*dt
   E = E-5*dt
-
+  if L < 0:
+    L = 0.
 
 plt.figure(figsize=(10,7))
 plt.plot(position[:,0],position[:,1])
